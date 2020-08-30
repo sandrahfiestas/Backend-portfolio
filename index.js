c537b80ce91d058ec8b5fe860c316eb99c98b780
sandrahfiestas <shfiestas@gmail.com>
2020-08-30 11:49:09 -0500

    creando puerto para renderizar data

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+//Creando el puerto
+const port = process.env.PORT || 8080;
+
+//Creando rutas
+const about = require('./json/about.json');
+const portfolio = require('./json/portfolio.json');
+
+
+app.get('/about', (req, res) => {
+    //renderiza
+    res.json(about)
+});
+
+app.get('/portfolio', (req, res) => {
+    res.json(portfolio)
+});
+
+app.listen(port, () => {
+    console.log(`Server on: ${port}`)
+});
